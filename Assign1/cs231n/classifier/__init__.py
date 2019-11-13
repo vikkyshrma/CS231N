d4040cb0768387366cab5dc40d0a07bc8cebefc8
vikkyshrma <55928837+vikkyshrma@users.noreply.github.com>
2019-11-13 13:54:33 +0530

    Update __init__.py

--- Assign1/cs231n/classifier/__init__.py
+++ Assign1/cs231n/classifier/__init__.py
@@ -1,2 +1,2 @@
-from cs231n.classifiers.k_nearest_neighbor import *
-from cs231n.classifiers.linear_classifier import *
+from cs231n.classifier.k_nearest_neighbor import *
+from cs231n.classifier.linear_classifier import *
